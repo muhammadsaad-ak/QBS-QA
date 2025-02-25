9d89723be98976a8c6c60e2ea8fbdc85214da34f
irfanahmed <irfanahmed.qbs@gmail.com>
2025-02-25 16:53:10 +0500

    LISTING UI OF ITEM CAVITY

---
+++ src/app/modules/admin/master-data/list-of-item-cavity/item-cavities/item-cavity-interface.ts
@@ -0,0 +1,6 @@
+export interface itemsCavityIF {
+    itemCode: string;
+    itemDescription: string;
+    mouldDescription: string;
+    noOfCavity: number;
+}
---
+++ src/app/modules/admin/master-data/list-of-item-cavity/list-of-item-cavity.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-list-of-item-cavity',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './list-of-item-cavity.component.html',
+  styleUrl: './list-of-item-cavity.component.scss'
+})
+export class ListOfItemCavityComponent {
+
+}
