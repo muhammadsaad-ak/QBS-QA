5612136ff4d1a424221651c4e8ee9624ceb4b7df
Ali Riaz <aliriaz0700@gmail.com>
2025-02-25 10:56:01 +0500

    Made listing table UI for Evaluation plan and routing of EP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -84,11 +84,15 @@ export const appRoutes: Route[] = [
                     { path: 'list-of-unit-measure-setup', loadChildren: () => import('app/modules/admin/master-data/list-of-unit-measure-setup/list-of-unit-measure-setup.routes') },
                     { path: 'list-of-items-samples', loadChildren: () => import('app/modules/admin/master-data/list-of-items-samples/list-of-items-samples.routes') },
                     { path: 'list-of-inspection-card', loadChildren: () => import('app/modules/admin/master-data/list-of-inspection-card/list-of-inspection-card.routes') },
-
                     { path: 'list-of-items-inspection-cards', loadChildren: () => import('app/modules/admin/master-data/list-of-items-inspection-cards/list-of-items-inspection-cards.routes') },
-
                     { path: 'list-of-testing-stepper',loadChildren: () => import('app/modules/admin/master-data/list-of-testing-stepper/testing-stepper.routes').then(m => m.TestingStepperRoutes)
 }                ]        
+            },
+            {
+                path: 'evaluation-plan', children: [
+                    { path: 'list-of-evaluation-plan', loadChildren: () => import('app/modules/admin/evaluation-plan/list-of-evaluation-plan/list-of-evaluation-plan.routes') },
+                    { path: 'list-of-sap-document', loadChildren: () => import('app/modules/admin/evaluation-plan/list-of-sap-document/list-of-sap-document.routes') },
+                ]
             },
 
         ]
---
+++ src/app/modules/admin/evaluation-plan/list-of-evaluation-plan/list-of-evaluation-plan.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ListOfEvaluationPlanComponent } from './list-of-evaluation-plan.component';
+
+export default [
+    {
+        path: '',
+        component: ListOfEvaluationPlanComponent,
+    },
+] as Routes;
