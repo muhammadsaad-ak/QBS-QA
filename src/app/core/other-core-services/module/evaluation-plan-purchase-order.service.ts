526cfb86e721f7fe9ad2b66fcd29ee262d1a39b9
irfanahmed <irfanahmed.qbs@gmail.com>
2025-03-18 12:11:49 +0500

    INTEGRATED API TO GET SAMPLE QTY FOR PLAN PURCHASE ORDER

--- src/app/core/other-core-services/module/evaluation-plan-purchase-order.service.ts
+++ src/app/core/other-core-services/module/evaluation-plan-purchase-order.service.ts
@@ -12,10 +12,12 @@ export class EvaluationPlanPurchaseOrderService {
   // BehaviorSubject to hold role data state
   private _listQualitativeResults = new BehaviorSubject<any[]>([]);
   private _itemIdSAP = new BehaviorSubject<any[]>([]);
+  private _sampleQty = new BehaviorSubject<any[]>([]);
 
   // Observable to expose role data state
   listQualitativeResults$: Observable<any[]> = this._listQualitativeResults.asObservable();
   itemIdSAP$: Observable<any[]> = this._itemIdSAP.asObservable();
+  sampleQty$: Observable<any[]> = this._sampleQty.asObservable();
 
 
 
@@ -70,6 +72,28 @@ export class EvaluationPlanPurchaseOrderService {
         }),
         catchError((error) => {
           console.error('ERROR WHILE FETCHING ITEM ID', error);
+          return throwError(error);
+        })
+      );
+  }
+  // GET SAMPLE QTY API
+  getSampleQuantityRange(inspectionQuantity: number, itemId: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.accessToken}`,
+      'Content-Type': 'application/json',
+    });
+
+    return this._httpClient
+      .get(`${environment.appApiUrl}/CSAPI/IPurchaseQCFeature/GetSampleQuantity?inspectionQuantity=${inspectionQuantity}&itemId=${itemId}`, { headers })
+      .pipe(
+        tap((sampleQuantity) => {
+          console.log('API RESPONSE:', sampleQuantity);
+          const fetchedSampleQuantity = (sampleQuantity as any).data ?? [];
+          this._sampleQty.next(fetchedSampleQuantity);
+          console.log('FETCHED RESPONSE', fetchedSampleQuantity);
+        }),
+        catchError((error) => {
+          console.error('ERROR WHILE FETCHING SAMPLE QTY', error);
           return throwError(error);
         })
       );
