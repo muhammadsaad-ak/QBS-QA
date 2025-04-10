500ffc7d1fc8f0339a5f3774b138c1d5e2580746
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2025-04-11 00:26:44 +0500

    stepper required field remove

--- src/app/modules/admin/master-data/list-of-testing-stepper/testing-stepper.component.ts
+++ src/app/modules/admin/master-data/list-of-testing-stepper/testing-stepper.component.ts
@@ -327,7 +327,7 @@ export class TestingStepperComponent implements AfterViewInit {
     });
     fifthFormGroup = this._formBuilder.group({
         id: [''],
-        intCode: ['', Validators.required],
+        intCode: [''],
         description: ['', Validators.required],
         isActive: [true],
         // type: ['', Validators.required],
